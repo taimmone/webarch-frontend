a14836d675323f19c7ae32490466d8a85f622056
Toni Immonen <toni.immonen@tuni.fi>
2021-04-08 17:23:37 +0300

    Initialize frontend with React and TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-black" aria-label="heading">
+        Make me a sandwich
+      </h1>
+    </div>
+  );
+};
+
+export default App;
