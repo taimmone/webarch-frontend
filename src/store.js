b99b8ea4f63b1723fe8ff30bdc1b3026d147b898
Toni Immonen <toni.immonen@tuni.fi>
2021-04-27 21:29:05 +0300

    Create actions for changing order state

--- src/store.js
+++ src/store.js
@@ -7,10 +7,17 @@ const { Provider } = store;
 const StateProvider = ({ children }) => {
   const [state, dispatch] = useReducer((state, action) => {
     switch (action.type) {
-      case 'selectSandwich':
+      case 'selectSandwich': {
         const { sandwichId } = action.payload;
         const newState = { ...state, sandwichId };
         return newState;
+      }
+      case 'setOrder':
+        const { order } = action.payload;
+        return { ...state, order };
+      case 'clearOrder': {
+        return { ...state, order: null };
+      }
       default:
         throw new Error();
     }
