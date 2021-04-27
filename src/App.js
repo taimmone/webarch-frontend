57803009bce2c4d1537bbccfd4f47aa42696bd2d
Toni Immonen <toni.immonen@tuni.fi>
2021-04-27 20:48:42 +0300

    Move "main" to its own component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import CartCheckout from './components/CartCheckout/CartCheckout';
 import Header from './components/Header';
+import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 
 const App = () => {
@@ -12,9 +13,7 @@ const App = () => {
       <Header toggleSidebar={toggleSidebar} />
       <div className="flex h-full text-center">
         <Sidebar open={openSidebar} toggleSidebar={toggleSidebar} />
-        <main className="flex-grow">
-          <div className="pt-8 text-4xl font-semibold">Content</div>
-        </main>
+        <Main />
         <CartCheckout />
       </div>
     </div>
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <main className="flex-grow flex flex-col mx-4">
+      <div className="pt-8 text-4xl font-semibold mb-12">Content</div>
+    </main>
+  );
+};
+
+export default Main;
