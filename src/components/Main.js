6447942586705f0c537fb638cc4575146bcd5337
Toni Immonen <toni.immonen@tuni.fi>
2021-04-27 21:08:29 +0300

    Make entire Sandwich component clickable

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import { useContext } from 'react';
 import { store } from '../store';
 
-const Sandwich = ({ sandwichId, handleSelect }) => {
+const Sandwich = ({ sandwichId, handleSelect, state }) => {
   return (
-    // TODO: Wrap with clickable area to toggle radio button
-    <div className="p-12 border text-2xl col-span-2 sm:col-auto font-semibold flex items-center justify-between">
-      <input type="radio" name="sandwich" onClick={() => handleSelect(sandwichId)} />
+    <div
+      className="p-12 border text-2xl col-span-2 sm:col-auto font-semibold flex items-center justify-between hover:border-black"
+      onClick={() => handleSelect(sandwichId)}
+    >
+      <input type="radio" name="sandwich" readOnly checked={sandwichId === state.sandwichId} />
       <h3>SandwichId: {sandwichId}</h3>
     </div>
   );
@@ -25,7 +27,7 @@ const Main = () => {
       <div className="pt-8 text-4xl font-semibold mb-12">Content</div>
       <div className="w-full grid grid-cols-2 gap-3 grid-flow-row">
         {sandwiches.map(({ sandwichId }) => {
-          return <Sandwich key={sandwichId} {...{ sandwichId, handleSelect }} />;
+          return <Sandwich key={sandwichId} {...{ sandwichId, handleSelect, state }} />;
         })}
       </div>
       <button className="self-end p-4 px-6 border-2 text-xl" type="button">
