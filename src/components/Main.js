b41d5852a5403e059d9035b5c0bdfdc0cc51e5bd
Toni Immonen <toni.immonen@tuni.fi>
2021-04-27 20:50:02 +0300

    Add dummy content for sandwiches

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,27 @@
+const Sandwich = ({ sandwichId }) => {
+  return (
+    // TODO: Wrap with clickable area to toggle radio button
+    <div className="p-12 border text-2xl col-span-2 sm:col-auto font-semibold flex items-center justify-between">
+      <input type="radio" name="sandwich" />
+      <h3>SandwichId: {sandwichId}</h3>
+    </div>
+  );
+};
+
 const Main = () => {
+  const sandwiches = [{ sandwichId: 0 }, { sandwichId: 1 }, { sandwichId: 2 }];
+
   return (
     <main className="flex-grow flex flex-col mx-4">
       <div className="pt-8 text-4xl font-semibold mb-12">Content</div>
+      <div className="w-full grid grid-cols-2 gap-3 grid-flow-row">
+        {sandwiches.map(({ sandwichId }) => {
+          return <Sandwich key={sandwichId} sandwichId={sandwichId} />;
+        })}
+      </div>
+      <button className="self-end p-4 px-6 border-2 text-xl" type="button">
+        Select sandwich ➤
+      </button>
     </main>
   );
 };
