00b5275a6f02f3a4a0860a46d1250f700b817fa8
Toni Immonen <toni.immonen@tuni.fi>
2021-04-29 19:45:19 +0300

    Remove bad logic

--- src/services/order.js
+++ src/services/order.js
@@ -1,7 +1,7 @@
 import 'axios';
 import axios from 'axios';
 
-const baseUrl = `${process.env.REACT_APP_API_URL}/order` || 'http://localhost:8080/order';
+const baseUrl = 'http://localhost:8080/order';
 
 const methods = {
   get: id => axios.get(`${baseUrl}/${id}`),
